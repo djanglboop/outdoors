f422cf554916e2e1aeab6305ae031ce932394db6
Yana Maximkina <djanglboop@gmail.com>
2019-11-06 11:29:03 +0900

    Adding a user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_05_071635) do
+ActiveRecord::Schema.define(version: 2019_11_06_021858) do
 
   create_table "journals", force: :cascade do |t|
     t.string "title"
@@ -44,6 +44,13 @@ ActiveRecord::Schema.define(version: 2019_11_05_071635) do
     t.text "description"
     t.string "smalltitle"
     t.text "destination"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
