dc87102a831ce48f2ecd4810cc683997b97ee717
Yana Maximkina <djanglboop@gmail.com>
2019-11-07 19:38:22 +0900

    Add sign up form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,17 +5,25 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.save
-            flash[:success] = "Welcome to the Great Outdoors Journals"
-            redirect_to journals_path
-        else
-            render 'new'
+    
+        respond_to do |format|
+          if @user.save
+            format.html { redirect_to root_path, notice: 'You was successfully signed up' }
+            format.json { render :show, status: :created, location: @user }
+          else
+            puts @user.errors.full_messages
+            format.html { render :new }
+            format.json { render json: @user.errors, status: :unprocessable_entity }
+          end
         end
+      end
+
+    def show
     end
 
     private
     def user_params
-        params.require(:user).permit(:username, :emai, :password)
+        params.require(:user).permit(:username, :email, :password)
     end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,8 @@ class User < ApplicationRecord
     has_many :journals
     before_save { self.email = email.downcase }
     validates :username, presence: true, uniqueness: { case_sensetive: false }, length: { minimum: 3, maximum: 30 }
-    VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+    VALID_EMAIL_REGEX = /\A[-\w.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, uniqueness: { case_sensetive: false },
     format: { with: VALID_EMAIL_REGEX }
     has_secure_password
-end
+end 
