0e6ac7dcb0855a5758dcb48ef07f73af5cd4f696
Yana Maximkina <djanglboop@gmail.com>
2019-11-11 11:37:23 +0900

    Add edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,15 @@
 class UsersController < ApplicationController
+
+    def show
+      @user = User.find(params[:id])
+    end
+
     def new
         @user = User.new
+    end
+
+    def edit
+      @user = User.find(params[:id])
     end
 
     def create
@@ -16,10 +25,22 @@ class UsersController < ApplicationController
             format.json { render json: @user.errors, status: :unprocessable_entity }
           end
         end
-      end
-
-    def show
     end
+
+  # PATCH/PUT /users/1
+  # PATCH/PUT /users/1.json
+  def update
+    @user = User.find(params[:id])
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'Profile was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
 
     private
     def user_params
